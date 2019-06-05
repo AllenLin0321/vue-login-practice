655fa5ef1b9a9fa5a87aed32924f82ef9b7b4d1c
Allen <glux7381@gmail.com>
2019-06-05 15:15:52 +0800

    Form Auth

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
+import Vuelidate from 'vuelidate'
+
 import Vue from 'vue'
 import App from './App.vue'
-
 import router from './router'
 import store from './store'
+
+Vue.use(Vuelidate)
 
 Vue.use(axios);
 axios.defaults.baseURL = 'https://vue-axios-9d321.firebaseio.com';
